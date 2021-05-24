c9c3b8bce326b1ec13709bf2b25f151f172b0ee2
JudeKLevine <77893132+JudeKLevine@users.noreply.github.com>
2021-05-25 00:36:11 +0200

    Update and rename ppcm_in_python.py to ppcm_to_pgcd.py

--- ppcm_to_pgcd.py
+++ ppcm_to_pgcd.py
@@ -1,20 +1,22 @@
+## cette methode permet de detreminer le pgcd de deux 
+## nombres en passant par leur ppcm
+
 def div(n):
     L = []
     for i in range(2, n + 1):
         while n%i == 0:
             n = n//i
             L.append(i)
-    return L
+    return L # renvoie la liste des diviseurs de n
 
 def produit(L):
     produit = 1
     for i in L:
         produit = produit*i
-    return produit
+    return produit # le produit des elements contenus dans le tableau
 
-def ppcm():
-    a = int(input("entrez la valeur de a : "))
-    b = int(input("entrez la valeur de b : "))
+def ppcm(a, b):
+
     c = div(a)
     d = div(b)
     for i in c :
@@ -22,3 +24,7 @@ def ppcm():
             if i == j:
                 d.remove(j)
     return produit(d)*produit(c)
+
+def pgcd(a, b):
+    return a * b // ppcm(a, b)
+# renvoie le pgcd de deux nombres
