c9deb887444df41944cd5e716dad84b629b4cf37
Jude Klevine <77893132+JudeKLevine@users.noreply.github.com>
2021-12-04 13:52:30 +0100

    Create Chiffrement_de_Cesar.py

---
+++ Arithmetic/Chiffrement_de_Cesar.py
@@ -0,0 +1,43 @@
+def code_une_(lettre):
+    a = ord(lettre) - 65
+    return a
+
+def decode_une_(carac):
+    a = chr(65 + carac)
+    return a
+
+def decode_mot(Liste):
+    b = list(map(decode_une_,Liste))
+    b = "".join(b)
+    return b
+
+def cesar(phrase,decimal):
+    L = []
+    B = []
+    for i in phrase:
+        a = code_une_(i) + decimal
+        if code_une_(i) == -33 :
+            e = code_une_(i) + 33
+            L.append(e)
+        else :
+            L.append(a)
+    for i in L :
+        if i == 0 :
+            B.append(-33)
+        else :
+            b = i%26
+            B.append(b)
+    c = decode_mot(B)
+    return c
+
+def decesar(Phrase,decimal):
+    L = []
+    a = list(map(code_une_,Phrase))
+    for i in a:
+        if i == -33:
+            L.append(-33)
+        else:
+            b = (i - decimal) % 26
+            L.append(b)
+    return decode_mot(L)
+print(decesar("VXLVVH",3))
