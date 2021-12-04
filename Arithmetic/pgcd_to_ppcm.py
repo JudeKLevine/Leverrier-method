f19c7397a62e8789152806f4121718382019da27
Jude Klevine <77893132+JudeKLevine@users.noreply.github.com>
2021-12-04 13:48:36 +0100

    Update pgcd_to_ppcm.py

--- Arithmetic/pgcd_to_ppcm.py
+++ Arithmetic/pgcd_to_ppcm.py
@@ -1,1 +1,13 @@
-fefeds
+## Cette programme permet de determiner le ppcm 
+## de deux nombres à partir de leur pgcd 
+
+def pgcd(a, b):
+    reste = a % b
+    while reste != 0:
+        a = b
+        b = reste
+        reste  = a % b
+    return b
+
+def ppcm(a, b):
+    return a * b // pgcd(a, b)
